c1ab07a7128f1c8433c4ac9f0c98bf1dd5716e97
Christian Alfoni <christianalfoni@gmail.com>
2022-05-10 12:49:47 +0200

    add initial react abstraction

--- src/react/index.test.tsx
+++ src/react/index.test.tsx
@@ -1,27 +1,164 @@
 import * as React from "react";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { createMachine, createStates } from "..";
+import { useMachine } from ".";
 
 const states = createStates({
   FOO: () => ({}),
   BAR: () => ({}),
+  BAZ: () => ({}),
 });
 
 const machine = createMachine(states, {
   FOO: {
     SWITCH: () => () => states.BAR(),
+    SWITCH_SAME: () => (state) => state,
   },
   BAR: {
     SWITCH: () => () => states.FOO(),
+    SWITCH_BAZ: () => () => states.BAZ(),
   },
+  BAZ: {},
 });
-
-const testHook = () => "hello world";
 
 describe("hooks", () => {
   it("should consume a machine through a hook", async () => {
-    const { result } = renderHook(() => testHook());
+    const { result } = renderHook(() => useMachine(machine, states.FOO()));
 
-    expect(result.current).toBe("hello world");
+    expect(result.current[0]).toEqual({ state: "FOO" });
+  });
+  it("should update hook state when machine changes state", async () => {
+    const { result } = renderHook(() => useMachine(machine, states.FOO()));
+
+    expect(result.current[0]).toEqual({ state: "FOO" });
+
+    act(() => {
+      result.current[1].SWITCH();
+    });
+
+    expect(result.current[0]).toEqual({ state: "BAR" });
+  });
+  it("should not reconcile when returning the same state", async () => {
+    let reconcileCount = 0;
+    const { result } = renderHook(() => {
+      reconcileCount++;
+      return useMachine(machine, states.FOO());
+    });
+
+    expect(result.current[0]).toEqual({ state: "FOO" });
+
+    act(() => {
+      result.current[1].SWITCH_SAME();
+    });
+
+    expect(reconcileCount).toBe(1);
+  });
+  it("should trigger effect when transitioning into initial state", async () => {
+    expect.assertions(1);
+    renderHook(() => {
+      const testMachine = useMachine(machine, states.FOO());
+      const [, , useTransitionEffect] = testMachine;
+
+      useTransitionEffect("FOO", () => {
+        expect(true).toBe(true);
+      });
+
+      return testMachine;
+    });
+  });
+  it("should handle single state effect", async () => {
+    let hasDisposedFooEffect = false;
+    let hasRunBarEffect = false;
+    const { result } = renderHook(() => {
+      const testMachine = useMachine(machine, states.FOO());
+      const [, , useTransitionEffect] = testMachine;
+
+      useTransitionEffect("FOO", () => () => {
+        hasDisposedFooEffect = true;
+      });
+      useTransitionEffect("BAR", () => {
+        hasRunBarEffect = true;
+      });
+
+      return testMachine;
+    });
+
+    act(() => {
+      result.current[1].SWITCH();
+    });
+
+    expect(hasDisposedFooEffect);
+    expect(hasRunBarEffect);
+  });
+  it("should handle multiple states effect", async () => {
+    let hasDisposedFooBarEffect = false;
+    let hasRunFooBarEffect = false;
+    const { result } = renderHook(() => {
+      const testMachine = useMachine(machine, states.FOO());
+      const [, , useTransitionEffect] = testMachine;
+
+      useTransitionEffect(["FOO", "BAR"], () => {
+        hasRunFooBarEffect = true;
+        return () => {
+          hasDisposedFooBarEffect = true;
+        };
+      });
+
+      return testMachine;
+    });
+
+    expect(hasRunFooBarEffect).toBe(true);
+
+    act(() => {
+      result.current[1].SWITCH();
+    });
+
+    expect(hasDisposedFooBarEffect).toBe(false);
+  });
+  it("should handle state with event effect", async () => {
+    let hasRunBarEffect = false;
+    const { result } = renderHook(() => {
+      const testMachine = useMachine(machine, states.FOO());
+      const [, , useTransitionEffect] = testMachine;
+
+      useTransitionEffect("BAR", "SWITCH", () => {
+        hasRunBarEffect = true;
+      });
+
+      return testMachine;
+    });
+
+    act(() => {
+      result.current[1].SWITCH();
+    });
+
+    expect(hasRunBarEffect).toBe(true);
+  });
+  it("should handle mulitple states with event effect", async () => {
+    let hasRunBarEffectCount = 0;
+    const { result } = renderHook(() => {
+      const testMachine = useMachine(machine, states.BAR());
+      const [, , useTransitionEffect] = testMachine;
+
+      useTransitionEffect(["FOO", "BAR"], "SWITCH", () => {
+        hasRunBarEffectCount++;
+      });
+
+      return testMachine;
+    });
+
+    expect(hasRunBarEffectCount).toBe(0);
+
+    act(() => {
+      result.current[1].SWITCH();
+    });
+
+    expect(hasRunBarEffectCount).toBe(1);
+
+    act(() => {
+      result.current[1].SWITCH();
+    });
+
+    expect(hasRunBarEffectCount).toBe(2);
   });
 });
--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,14 +1,148 @@
-import * as React from 'react'
-import { StateMachine } from "..";
+import * as React from "react";
+import {
+  StateMachineCreator,
+  StateMachine,
+  TStateCreator,
+  TStateCreatosrWithState,
+  TTransitions,
+} from "..";
+import { createMachine, createStates } from "..";
 
-export const useMachine = <T extends StateMachine<any, any>>(spawn: T, initialState: ReturnType<ReturnType<T>["getState"]>) => {
-    const [machine] = React.useState(() => spawn(initialState))
-    const [state, setState] = React.useState(machine.getState())
-    const useTransitionEffect = React.useCallback(() => {
-        
-    }, [])
-    
-    React.useEffect(() => machine.subscribe(setState), [])
-    
-    return [state, machine.events, useTransitionEffect]
+export type UseTransitionEffectWithEvent<
+  States extends Record<string, TStateCreator>,
+  T extends TTransitions<States>
+> = <
+  SS extends keyof States | (keyof States)[],
+  EE extends {
+    [U in keyof T]: {
+      [E in keyof T[U]]: E;
+    }[keyof T[U]];
+  }[keyof T]
+>(
+  current: SS,
+  event: EE,
+  effect: (
+    state: SS extends (keyof States)[]
+      ? ReturnType<States[SS[number]]>
+      : SS extends keyof States
+      ? ReturnType<States[SS]>
+      : never,
+    ...eventParams: SS extends (keyof States)[]
+      ? Parameters<T[SS[number]][EE]>
+      : SS extends keyof States
+      ? Parameters<T[SS][EE]>
+      : never
+  ) => void
+) => void;
+
+type UseTransitionEffect<States extends Record<string, TStateCreator>> = <
+  SS extends keyof States | (keyof States)[]
+>(
+  current: SS,
+  effect: (
+    state: SS extends (keyof States)[]
+      ? ReturnType<States[SS[number]]>
+      : SS extends keyof States
+      ? ReturnType<States[SS]>
+      : never
+  ) => void | (() => void)
+) => void;
+
+const createUseTransitionEffect =
+  <
+    States extends Record<string, TStateCreator>,
+    T extends TTransitions<States>
+  >(
+    machine: StateMachine<States, T>
+  ): UseTransitionEffect<States> & UseTransitionEffectWithEvent<States, T> =>
+  (...params: any[]) => {
+    const state = params[0];
+    const eventType = params[1];
+    const cb = params[2] || params[1];
+
+    if (typeof eventType === "string") {
+      React.useEffect(
+        () =>
+          machine.subscribe((currentState, event) => {
+            const isState = Array.isArray(state)
+              ? state.includes(currentState.state)
+              : currentState.state === state;
+            if (isState && event.type === eventType) {
+              cb(currentState, ...event.params);
+            }
+          }),
+        []
+      );
+    } else {
+      React.useEffect(() => {
+        let subscriptionDisposer: (() => void) | undefined;
+        const disposer = machine.subscribe((currentState, _, prevState) => {
+          if (Array.isArray(state)) {
+            const hasChangedWithinStates =
+              state.includes(currentState.state) &&
+              state.includes(prevState?.state);
+            const hasChangedToState =
+              state.includes(currentState.state) &&
+              !state.includes(prevState?.state);
+
+            if (hasChangedWithinStates) {
+              return;
+            }
+
+            if (hasChangedToState) {
+              subscriptionDisposer = cb(currentState);
+            } else {
+              subscriptionDisposer?.();
+              subscriptionDisposer = undefined;
+            }
+          } else {
+            const hasChangedToState =
+              currentState.state === state && prevState?.state !== state;
+
+            if (hasChangedToState) {
+              subscriptionDisposer = cb(currentState);
+            } else {
+              subscriptionDisposer?.();
+              subscriptionDisposer = undefined;
+            }
+          }
+        });
+
+        if (
+          Array.isArray(state)
+            ? state.includes(machine.getState().state)
+            : machine.getState().state === state
+        ) {
+          subscriptionDisposer = cb(machine.getState());
+        }
+
+        return () => {
+          disposer();
+          subscriptionDisposer?.();
+        };
+      }, []);
+    }
+  };
+
+export const useMachine = <T extends StateMachineCreator<any, any>>(
+  spawn: T,
+  initialState: ReturnType<ReturnType<T>["getState"]>
+): T extends StateMachineCreator<infer States, infer Transitions>
+  ? [
+      ReturnType<ReturnType<T>["getState"]>,
+      ReturnType<T>["events"],
+      UseTransitionEffect<States> &
+        UseTransitionEffectWithEvent<States, Transitions>
+    ]
+  : never => {
+  const [machine] = React.useState(() => spawn(initialState));
+  const [state, setState] = React.useState(machine.getState());
+  const useTransitionEffect = React.useMemo(
+    () => createUseTransitionEffect(machine),
+    [machine]
+  );
+
+  React.useEffect(() => machine.subscribe(setState), []);
+
+  return [state, machine.events, useTransitionEffect] as any;
 };
